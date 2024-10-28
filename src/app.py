0e6f6c8f1e118a9e5cc1ae94bf3b55e8f7ed7e88
capgalea <galea.charlesa@gmail.com>
2024-10-28 17:31:50 +1100

    added map and buttons to select all or city or company

--- src/app.py
+++ src/app.py
@@ -5,14 +5,30 @@ from PyPDF2 import PdfReader
 import pandas as pd
 import numpy as np 
 import spacy
+import plotly.express as px
+from streamlit_plotly_events import plotly_events
 from langchain_community.embeddings.spacy_embeddings import SpacyEmbeddings
 
-
+# Set the page configuration
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 st.set_page_config("Australian BioTech")
 
+# Function to load the data
+@ st.cache_data
+def load_csv(file):
+    df = pd.read_csv(file)
+    return df
+
+# Function to convert URLs to Markdown hyperlinks
+def make_clickable(url):
+    return f'<a href="{url}" target="_blank">{url}</a>'
+
+# Load the data
+data = load_csv("data/bioTech_data.csv")
+
+# Read the PDF files
 def pdf_read(pdf_doc):
     text = ""
     for pdf in pdf_doc:
@@ -21,18 +37,134 @@ def pdf_read(pdf_doc):
             text += page.extract_text()
     return text
 
-# # Load environment variables from .env file
-# load_dotenv()
-
-# # Sidebar inputs
-# openai_api_key = st.sidebar.text_input("Enter OpenAI API Key", os.getenv("OPENAI_API_KEY", ""))
-# temperature = st.sidebar.slider("Temperature", 0.0, 1.0, 0.2)
 
 def main():
     def main_page():
+        #Create two columns
+        col1, col2 = st.columns([0.5, 10])
 
-        st.title("Welcome to the Australian BioTech App")
-        st.write("This is the main page. Use the sidebar to navigate to other pages.")
+        with col1:
+            # Add an image to the left column
+            st.image("images/AusBioTech_logo.png", width=100)   
+
+        with col2:
+            # Add a header to the right column
+            st.markdown("<h1 style='text-align: center;'>Australian Biotechnology Companies</h1>", unsafe_allow_html=True)
+
+        # Add vertical space between header and plots
+        st.write("")
+        st.write("")
+
+        # Display the data
+        # Create two columns
+        col1, col2 = st.columns([2, 2])
+
+        with col1:
+            # Create bar plotly bar chart showing the number of companies by city
+            st.write("Number of Companies by State")
+            city_counts = data["City"].value_counts()
+            st.bar_chart(city_counts)
+
+        # Create a sidebar to choose the company or city
+        choice = st.sidebar.radio("Menu", ["Company", "City"])
+
+
+        if choice == "City":
+            # Create table showing the selected city
+            city_list = data["City"].unique().tolist()
+            selected_city = st.sidebar.selectbox("Select a City", city_list, index=None) 
+            
+            # Create checkbox to show all data
+            if st.sidebar.checkbox("Show All Data"):
+                # Display the data in an expander
+                with st.expander("View Data"):
+                    st.dataframe(data,
+                                 column_config={"Website2": st.column_config.LinkColumn("Company Website")}
+                                 )
+                    df = data
+            else:
+                # Display the data in an expander
+                with st.expander("View Data"):
+                    city_df = data[data["City"] == selected_city]
+                    st.dataframe(city_df,
+                                 column_config={"Website2": st.column_config.LinkColumn("Company Website")}
+                                 )
+                    df = city_df
+            
+
+        elif choice == "Company":   
+            company_list = data["Companies"].unique().tolist()
+            selected_company = st.sidebar.selectbox("Select a Company", company_list, index=None) 
+
+            # Create checkbox to show all data
+            if st.sidebar.checkbox("Show All Data"):
+                # Display the data in an expander
+                with st.expander("View Data"):
+                    st.dataframe(data,
+                                 column_config={"Website2": st.column_config.LinkColumn("Company Website")}
+                                 )
+                    df = data
+            else:
+                # Display the data in an expander
+                with st.expander("View Data"):
+                    company_df = data[data["Companies"] == selected_company]
+                    st.dataframe(company_df,
+                                 column_config={"Website2": st.column_config.LinkColumn("Company Website")}
+                                 )
+                    df = company_df
+            
+
+        with col2:
+            # Create plotly map showing the location of each company using the latitude and lognitude columns
+            # Ensure the data contains latitude and longitude columns
+            if 'Latitude' not in data.columns or 'Longitude' not in data.columns:
+                st.error("The data does not contain latitude and longitude information.")
+            elif choice == "Company":
+                    # Create a Plotly map
+                    fig = px.scatter_mapbox(
+                    df,
+                    lat="Latitude",
+                    lon="Longitude",
+                    hover_name="Companies",
+                    hover_data={"Latitude": False, "Longitude": False, "Location": True, "City": True, "Website2": True},
+                    zoom=2,
+                    height=300,
+                    center={"lat": -25.2744, "lon": 133.7751}  # Center on Australia
+                    )
+
+                    fig.update_layout(mapbox_style="open-street-map")
+                    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+
+                    # Display the map in the Streamlit app
+                    st.plotly_chart(fig, key="map")
+            
+            elif choice == "City":
+                    # Create a Plotly map
+                    fig = px.scatter_mapbox(
+                    df,
+                    lat="Latitude",
+                    lon="Longitude",
+                    hover_name="Companies",
+                    hover_data={"Latitude": False, "Longitude": False, "Location": True, "City": True, "Website2": True},
+                    zoom=2,
+                    height=300,
+                    center={"lat": -25.2744, "lon": 133.7751}  # Center on Australia
+                    )
+        
+                    fig.update_layout(mapbox_style="open-street-map")
+                    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+        
+        
+                    # Display the map in the Streamlit app
+                    st.plotly_chart(fig, key="map")
+
+        
+
+        
+ 
+
+
 
     def second_page():
 
