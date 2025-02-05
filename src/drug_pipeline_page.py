b83ba2a767b2837524a95161107f0c7334e4adc7
capgalea <galea.charlesa@gmail.com>
2025-02-05 22:13:10 +1100

    Change filepath to csv file

--- src/drug_pipeline_page.py
+++ src/drug_pipeline_page.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 def load_data():
     # Load the drug pipeline data
-    df = pd.read_csv(r'C:\Users\guino\OneDrive\Documents\GitHub\ausbio_streamlit_LLM\data\Drug_Pipelines.csv')
+    df = pd.read_csv(r'\data\Drug_Pipelines.csv')
     return df
 
 def drug_pipeline_page():
